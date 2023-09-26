174f44e3a2427a308121e6e1fe040e0e80332dae
Ivo Oliveira <ivo793@gmail.com>
2023-09-25 23:54:35 -0300

    primeiros passos do dashboard

---
+++ script/script.js
@@ -0,0 +1,88 @@
+//parte da fetch (API)
+document.addEventListener('click', async (e) => {
+    const el = e.target;
+    const tag = el.tagName.toLowerCase();
+
+    if (tag === 'a') {
+        e.preventDefault();
+        await carregaPagina(el);
+    }
+});
+
+async function carregaPagina(el) {
+    try {
+        const href = el.getAttribute('href');
+        const response = await fetch(href);
+
+        if (response.status !== 200) {
+            throw new Error('Erro na requisição: ' + response.status);
+        }
+
+        const contentType = response.headers.get('content-type');
+        if (!contentType || !contentType.includes('text/html')) {
+            throw new Error('A resposta não é uma página HTML válida.');
+        }
+
+        const html = await response.text();
+        carregaResultado(html);
+    } catch (e) {
+        console.error('Erro ao carregar a página:', e);
+    }
+}
+
+function carregaResultado(response) {
+    const resultado = document.querySelector('body');
+    resultado.innerHTML = response;
+}
+
+
+//lista de games
+function toggleMenu(toShow,toHidden){
+    toShow.style.display = 'none';
+    toHidden.style.display = 'flex';
+}
+
+function clickPlay1(){
+    toggleMenu(play2,play1)
+}   
+
+function clickPlay2(){
+    toggleMenu(play1,play2)
+}
+
+
+
+
+//Social
+function clickMenu(element){
+    if(element.style.display === 'grid'){
+        element.style.display = 'none'
+    }else{
+        element.style.display ='grid'
+    }  
+}
+
+function clickOnline(){
+    clickMenu(online)
+}
+
+function clickOffiline(){
+    clickMenu(offline)
+}
+
+function clickGroup(){
+    clickMenu(group)
+}
+
+
+
+
+
+//exemplo
+/*function clickSocial(){
+    if(social.style.height >= "57%" || social.style.height == "100%"){
+        social.style.height = "18%";
+    }else{
+        social.style.height = "57%"
+    }
+}*/
