5119e40092d49accf69feef6442c4c347e057bb3
Ivo Oliveira <ivo793@gmail.com>
2023-09-27 23:07:30 -0300

    adicionando slide na página principal e alterando o design dos cards.

--- script/script.js
+++ script/script.js
@@ -36,6 +36,57 @@ function carregaResultado(response) {
 }
 
 
+
+/*slide principal*/
+
+let list = document.querySelector('.list');
+let items = document.querySelectorAll('.item');
+let dots = document.querySelectorAll('.dots li');
+let prev = document.getElementById('prev');
+let next = document.getElementById('next');
+
+let active = 0;
+let lengthItems = items.length - 1;
+
+next.onclick = function(){
+    if(active + 1 > lengthItems){
+        active = 0;
+    }else{
+        active = active + 1;
+    }
+    reloadSlider();
+}
+
+prev.onclick = function(){
+    if(active - 1 < 0){
+        active = lengthItems;
+    }else{
+        active = active - 1;
+    }
+    reloadSlider();
+}
+
+let refreshSlider = setInterval(()=> {next.click()}, 5000);
+
+function reloadSlider(){
+    let checkLeft = items[active].offsetLeft;
+    list.style.left = -checkLeft + 'px';
+
+    let lastActiveDot = document.querySelector('.slider .dots li.active');
+    lastActiveDot.classList.remove('active');
+    dots[active].classList.add('active');
+    clearInterval(refreshSlider);
+    refreshSlider = setInterval(()=> {next.click()}, 5000);
+}
+dots.forEach((li, key) =>{
+    li.addEventListener('click', function(){
+        active = key;
+        reloadSlider();
+    })
+})   
+
+
+
 //lista de games
 function toggleMenu(toShow,toShow2,toHidden){
     toShow.style.display = 'none';
